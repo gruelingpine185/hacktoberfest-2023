92f6171d9fb166f249b93b79e34463846f377c30
ROHITH PRADEEP <142139740+ROHITTTTZ@users.noreply.github.com>
2023-10-02 17:06:57 +0530

    Update hw.java

--- hello_world/hw.java
+++ hello_world/hw.java
@@ -1,3 +1,6 @@
+//in terminal run - "javac hw.java" to compile
+// run - "java hw" to run the program 
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HELLO WORLD !");
